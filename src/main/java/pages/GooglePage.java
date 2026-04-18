28c278520638b43e3d6a12a6e6947a8862b3d434
*Mike <mljimenez983@gmail.com>
2026-04-18 17:09:11 -0400

    Adding ExpectedConditions and Waits

--- src/main/java/pages/GooglePage.java
+++ src/main/java/pages/GooglePage.java
@@ -3,6 +3,8 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class GooglePage {
 
